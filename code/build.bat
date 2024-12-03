33174699440389cf9b5556413d9fb974b5dbf4c7
Frostbyte8 <51795393+Frostbyte8@users.noreply.github.com>
2024-12-03 11:39:23 -0500

    Updated build system.

--- code/build.bat
+++ code/build.bat
@@ -8,8 +8,26 @@ if NOT exist "..\build" (
 
 pushd ..\build
 
-REM NOTE: /EHsc is just for debug builds
+:: Set Compiler Settings
 
-cl /nologo /Zi /FC /EHsc ..\code\main.cpp ..\code\win32pp\main_window.cpp /link /SUBSYSTEM:WINDOWS user32.lib comctl32.lib Ole32.lib Gdi32.lib Advapi32.lib
+set libsShared=user32.lib comctl32.lib Ole32.lib Gdi32.lib Advapi32.lib
+
+set compFlagsDebug=/nologo /Zi /FC /EHsc
+set linkFlagsDebug=/SUBSYSTEM:WINDOWS
+set libsDebug=%libsShared%
+
+:: Source Files
+
+set sources=
+for /f "tokens=*" %%A in (..\code\sources.txt) do call :appendSourcesStart %%A 
+goto appendSourcesEnd
+
+:appendSourcesStart
+set sources=%sources%..\code\%1 
+goto :eof
+
+:appendSourcesEnd
+
+cl %compFlagsDebug% %sources% /link %linkFlagsDebug% %libsDebug%
 
 popd
