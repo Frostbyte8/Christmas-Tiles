0d580ae3aa3a443908319bf89d47d4f016dbdc90
Frostbyte8 <51795393+Frostbyte8@users.noreply.github.com>
2024-12-04 17:30:46 -0500

    Fixed up the build system a bit. Now it will mark the files as stale if the compilation fails. Still not perfect, but still better than what we had.
    Also MSDN has errors on their pages about some of this stuff. It is
    insane.

--- code/build.bat
+++ code/build.bat
@@ -30,38 +30,46 @@ goto :eof
 :appendSourcesEnd
 
 "../misc/lastmodified.exe" %sources% > "../misc/current.txt"
-::comm.exe -13 "../misc/current.txt" "../misc/stale.txt" > ../misc/_filech_temp.txt
+comm.exe -13 "../misc/current.txt" "../misc/stale.txt" > ../misc/_filech_temp.txt
 
-::set sources=
+set sources=
 
-::for /f "tokens=*" %%A in (..\misc\_filech_temp.txt) do call :getChangedSourcesStart %%A 
-::goto :getChangedSourcesEnd
+for /f "tokens=*" %%A in (..\misc\_filech_temp.txt) do call :getChangedSourcesStart %%A 
+goto :getChangedSourcesEnd
 
-:::getChangedSourcesStart
-::set sources=%sources% %1
-::goto :eof
+:getChangedSourcesStart
+set sources=%sources% %1
+goto :eof
 
-:::getChangedSourcesEnd
+:getChangedSourcesEnd
 
-::if exist "../misc/stale.txt" (
-::    del "../misc/stale.txt"
-::)
+if exist "../misc/_filech_temp.txt" (
+    del "../misc/_filech_temp.txt"
+)
 
-::if exist "../misc/_filech_temp.txt" (
-::    del "../misc/_filech_temp.txt"
-::)
+if not defined sources ( 
+    echo No relevant changes detected.
+) else (
+REM Ugh MSDN doesn't mention in their docs that you need to first
+REM turn on EnableDelayedExpansion, and then get the value, otherwise, it
+REM will always be 0.
+    setlocal EnableDelayedExpansion
+    cl %compFlagsDebug% %sources%
+    if !errorlevel! equ 0 goto compileSuccess
+    endlocal
+    popd
+    echo Compile Failed!
+    goto :EOF
+:compileSuccess
+    endlocal
+    link %linkFlagsDebug% *.obj
+)
 
-::ren "../misc/current.txt" "../misc/stale.txt"
 
-::echo %sources%
-::if not defined sources ( 
-::    echo No relevant changes detected.
-::) else (
-::    cl %compFlagsDebug% %sources%
-::    link %linkFlagsDebug% *.obj
-::)
+if exist "../misc/stale.txt" (
+    del "../misc/stale.txt"
+)
 
-cl %compFlagsDebug% %sources%
-link %linkFlagsDebug% *.obj
+ren "../misc/current.txt" "../misc/stale.txt"
 
 popd
