24e82a48ee22c0ec3d5db8dd00cd3a2c183beab1
Frostbyte8 <51795393+Frostbyte8@users.noreply.github.com>
2024-12-03 19:56:24 -0500

    Updated build system again. Now it only recompiles changes that actually matter. Not perfect yet, but it'll do.

--- code/build.bat
+++ code/build.bat
@@ -12,9 +12,10 @@ pushd ..\build
 
 set libsShared=user32.lib comctl32.lib Ole32.lib Gdi32.lib Advapi32.lib
 
-set compFlagsDebug=/nologo /Zi /FC /EHsc
-set linkFlagsDebug=/SUBSYSTEM:WINDOWS
+set compFlagsDebug=/nologo /c /Zi /FC /EHsc
+
 set libsDebug=%libsShared%
+set linkFlagsDebug=/nologo /SUBSYSTEM:WINDOWS /DEFAULTLIB:%libsDebug%
 
 :: Source Files
 
@@ -23,11 +24,41 @@ for /f "tokens=*" %%A in (..\code\sources.txt) do call :appendSourcesStart %%A
 goto appendSourcesEnd
 
 :appendSourcesStart
-set sources=%sources%..\code\%1 
+set sources=%sources%"..\code\%1" 
 goto :eof
 
 :appendSourcesEnd
 
-cl %compFlagsDebug% %sources% /link %linkFlagsDebug% %libsDebug%
+"../misc/lastmodified.exe" %sources% > "../misc/current.txt"
+"../misc/comm.exe" -13 "../misc/current.txt" "../misc/stale.txt" > ../misc/_filech_temp.txt
+
+set sources=
+
+for /f "tokens=*" %%A in (..\misc\_filech_temp.txt) do call :getChangedSourcesStart %%A 
+goto :getChangedSourcesEnd
+
+:getChangedSourcesStart
+set sources=%sources%%1
+goto :eof
+
+:getChangedSourcesEnd
+
+if exist "../misc/stale.txt" (
+    del "../misc/stale.txt"
+)
+
+if exist "../misc/_filech_temp.txt" (
+    del "../misc/_filech_temp.txt"
+)
+
+ren "../misc/current.txt" "../misc/stale.txt"
+
+echo %sources%
+if not defined sources ( 
+    echo No relevant changes detected.
+) else (
+    cl %compFlagsDebug% %sources%
+    link %linkFlagsDebug% *.obj
+)
 
 popd
